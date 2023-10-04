19f5e2270fd2176881308632121ab2245ca7ed30
Beknazar <beknazarzholdoshbekov1@gmail.com>
2023-10-04 21:56:03 +0600

    added reusable colors

---
+++ src/components/UI/Colors.jsx
@@ -0,0 +1,23 @@
+export const Primary = {
+   white: '#FFFFFF',
+   black: '#222222',
+   charcoal: '#4D4D51',
+   lightLime: 'linear-gradient(#0CBB6B, #027B44)',
+   green: '#048741',
+   background: '#F5F5F5',
+}
+export const Secondary = {
+   input: '#D9D9D9',
+   silver: '#E0E2E7',
+   lightGray: 'linear-gradient(#FDFDFD, #E4E7EE)',
+   white: '#F3F1F1',
+   gray: '#959595',
+   brown: '#E4772F',
+}
+export const Tertiary = {
+   azure: '#FFFFFF',
+   mint: '#222222',
+   red: '#F91515',
+   skyBlue: '#3977C0',
+   blue: '#346EFB',
+}

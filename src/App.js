cb0218a38dbbb4c98fdb121fc70deae41019ae75
Ali Abdukadyrov <vip.astiali@gmail.com>
2023-10-05 17:45:35 +0600

    added input UI component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import { InputUi } from './components/UI/input/InputUi'
 
 function App() {
-   return <div className="App">App</div>
+   return (
+      <div className="App">
+         <InputUi size="small" />
+      </div>
+   )
 }
 
 export default App
---
+++ src/components/UI/input/InputUi.jsx
@@ -0,0 +1,28 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+export const InputUi = ({ size }) => {
+   const InputStyled = {
+      '& .MuiOutlinedInput-root': {
+         width: '414px',
+         height: size === 'small' ? '38px' : '45px',
+         borderRadius: '8px',
+         '& fieldset': {
+            borderColor: '#D9D9D9',
+         },
+         '&:hover fieldset': {
+            borderColor: '#959595',
+         },
+         '&:active fieldset': {
+            borderColor: '#rgba(4, 135, 65, 0.80)',
+         },
+         '&:invalid fieldset': {
+            borderColor: '#F91515',
+         },
+         '&.Mui-focused fieldset': {
+            borderColor: 'rgba(4, 135, 65, 0.80)',
+         },
+      },
+   }
+   return <TextField sx={InputStyled} />
+}

8c0810cf5ea79e23517d5247344472462d3d08d2
Ali Abdukadyrov <vip.astiali@gmail.com>
2023-10-11 17:30:53 +0600

    added commets

--- src/components/UI/input/InputUi.jsx
+++ src/components/UI/input/InputUi.jsx
@@ -1,47 +1,51 @@
 import { TextField } from '@mui/material'
 import React from 'react'
 
-export const Input = ({
-   value,
-   ref,
-   onChange,
-   placeholder,
-   invalid,
-   error,
-   height = '45px',
-   width = '414px',
-}) => {
-   const InputStyled = {
-      '& .MuiOutlinedInput-root': {
-         width: { width },
-         height: { height },
-         borderRadius: '8px',
-         '& fieldset': {
-            borderColor: '#D9D9D9',
-         },
-         '&:hover fieldset': {
-            borderColor: '#959595',
-         },
-         '&:active fieldset': {
-            borderColor: '#rgba(4, 135, 65, 0.80)',
-         },
-         '&:invalid fieldset': {
-            borderColor: '#F91515',
-         },
-         '&.Mui-focused fieldset': {
-            borderColor: 'rgba(4, 135, 65, 0.80)',
-         },
+export const Input = React.forwardRef(
+   (
+      {
+         value,
+         onChange,
+         placeholder,
+         invalid,
+         error,
+         height = '3rem',
+         width = '26rem',
       },
+      ref
+   ) => {
+      const InputStyled = {
+         '& .MuiOutlinedInput-root': {
+            width: { width },
+            height: { height },
+            borderRadius: '8px',
+            '& fieldset': {
+               borderColor: '#D9D9D9',
+            },
+            '&:hover fieldset': {
+               borderColor: '#959595',
+            },
+            '&:active fieldset': {
+               borderColor: '#rgba(4, 135, 65, 0.80)',
+            },
+            '&:invalid fieldset': {
+               borderColor: '#F91515',
+            },
+            '&.Mui-focused fieldset': {
+               borderColor: 'rgba(4, 135, 65, 0.80)',
+            },
+         },
+      }
+      return (
+         <TextField
+            sx={InputStyled}
+            value={value}
+            ref={ref}
+            onChange={onChange}
+            placeholder={placeholder}
+            error={Boolean(error)}
+            invalid={invalid}
+         />
+      )
    }
-   return (
-      <TextField
-         sx={InputStyled}
-         value={value}
-         inputRef={ref}
-         onChange={onChange}
-         placeholder={placeholder}
-         error={Boolean(error)}
-         invalid={invalid}
-      />
-   )
-}
+)

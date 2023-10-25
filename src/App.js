55acff5f5c4e22be525e73171f71ed8ab65f82ee
Beknazar <beknazarzholdoshbekov1@gmail.com>
2023-10-25 13:03:48 +0600

    added themes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css'
-import { Input } from './components/UI/input/Input'
 
 function App() {
-   return (
-      <div className="App">
-         <Input />
-      </div>
-   )
+   return <div className="App">App </div>
 }
 
 export default App

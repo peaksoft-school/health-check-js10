7399fd40b8baae8ab76ce9d21237e7990c49624e
Beknazar <beknazarzholdoshbekov1@gmail.com>
2023-11-03 17:18:20 +0600

    test

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const AppRoutes = () => {
+   return (
+      <Routes>
+         <Route path='/' element={} />
+      </Routes>
+   )
+}
+
+export default AppRoutes

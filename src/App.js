3eaf42cef6f0ba917e3ede1f0ef77e79fd23e44a
Adik <aidynbek.aliuulu@gmail.com>
2023-11-08 15:28:34 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import './App.css'
+import CheckboxUI from './components/UI/Checkbox'
+import { Input } from './components/UI/input/Input'
+import Header from './layout/admin/header/Header'
 
 function App() {
-   return <div className="App">App</div>
+   return (
+      <>
+         <div style={{ backgroundColor: '#F5F5F5' }}>
+            <Header />
+         </div>
+         <div>
+            <Input />
+            <CheckboxUI />
+         </div>
+      </>
+   )
 }
 export default App

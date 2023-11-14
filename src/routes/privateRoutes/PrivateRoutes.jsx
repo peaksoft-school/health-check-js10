29e214bae3c4d70e366cff33e5d09e22eeded7a9
Beknazar <beknazarzholdoshbekov1@gmail.com>
2023-11-14 11:17:28 +0600

    changed routes.login on homepage

--- src/routes/privateRoutes/PrivateRoutes.jsx
+++ src/routes/privateRoutes/PrivateRoutes.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
-import { routes } from '../../utils/constants/constants'
 
 export const PrivateRoutes = ({ component, isAuth }) => {
    if (isAuth) {
       return component
    }
-   return <Navigate to={routes.LOGIN} replace />
+   return <Navigate to="/homepage" replace />
 }

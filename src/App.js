5553710291cbe668a98531d040f41cac8bb1d3dc
karachachit <karachachit@gmail.com>
2023-12-20 16:17:37 +0600

    test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { pdfjs } from 'react-pdf'
 import { applicationsThunk } from './store/applications/applicationsThunk'
-
-import GetResult from './components/GetResult'
 import { login } from './store/auth/authSlice'
 import { USER_KEY } from './utils/constants/constants'
 // eslint-disable-next-line import/no-unresolved
 import 'react-pdf/dist/Page/TextLayer.css'
+import GetResult from './components/GetResult'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 
